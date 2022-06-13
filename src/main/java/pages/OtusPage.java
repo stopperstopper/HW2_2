5baeb74b9dc0c48559babf6cf6631d7e4f7dd18a
Alex <kvirell@mail.ru>
2022-06-13 12:19:38 +0500

    any fix

--- src/main/java/pages/OtusPage.java
+++ src/main/java/pages/OtusPage.java
@@ -4,15 +4,14 @@ import components.MenuBar;
 import org.openqa.selenium.WebDriver;
 
 public class OtusPage extends BasePage<OtusPage> {
-    private static final String PATH_NAME = "";
-    private MenuBar menuBar;
 
-    public OtusPage(WebDriver driver) {
-        super(driver, PATH_NAME);
-        menuBar = new MenuBar(driver);
-    }
+  private MenuBar menuBar;
+  public OtusPage(WebDriver driver) {
+    super(driver, "");
+    menuBar = new MenuBar(driver);
+  }
 
-    public MenuBar getHeaderMenu() {
-        return menuBar;
-    }
+  public MenuBar getHeaderMenu() {
+    return menuBar;
+  }
 }
